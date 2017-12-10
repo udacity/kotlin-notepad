c15c080863f5d53af2b60ebaadd9abfc33ac9e99
Taji <codedalchemydesign@gmail.com>
2017-12-10 13:39:52 -0500

    Modified Note.kt to be a Kotlin Data Class.

--- app/src/main/java/com/udacity/notepad/data/Note.kt
+++ app/src/main/java/com/udacity/notepad/data/Note.kt
@@ -2,10 +2,10 @@ package com.udacity.notepad.data
 
 import java.util.*
 
-class Note {
-    var id = -1
-    var text: String? = null
-    var isPinned = false
-    var createdAt = Date()
+data class Note(
+        var id: Int = -1,
+        var text: String? = null,
+        var isPinned: Boolean = false,
+        var createdAt: Date = Date(),
     var updatedAt: Date? = null
-}
+)
