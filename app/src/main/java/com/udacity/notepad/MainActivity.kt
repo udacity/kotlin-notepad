3b678a849bfd443e7ea3f3ab78261a8ae9bf8d7c
Taji <codedalchemydesign@gmail.com>
2017-12-09 19:15:10 -0500

    Use kotlin extensions to get rid of find by views.

--- app/src/main/java/com/udacity/notepad/MainActivity.kt
+++ app/src/main/java/com/udacity/notepad/MainActivity.kt
@@ -1,35 +1,29 @@
 package com.udacity.notepad
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import com.udacity.notepad.crud.CreateActivity
 import com.udacity.notepad.recycler.NotesAdapter
 import com.udacity.notepad.util.SpaceItemDecoration
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var recycler: RecyclerView? = null
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { startActivity(CreateActivity.get(this@MainActivity)) }
 
-        recycler = findViewById(R.id.recycler)
-        recycler!!.layoutManager = LinearLayoutManager(this)
-        recycler!!.addItemDecoration(SpaceItemDecoration(this, R.dimen.margin_small))
-        recycler!!.adapter = NotesAdapter(this)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.addItemDecoration(SpaceItemDecoration(this, R.dimen.margin_small))
+        recycler.adapter = NotesAdapter(this)
     }
 
     override fun onResume() {
